2b74c170c43a4467d3fb294b05769ca5475bd1dc
roopkadam <ruprani.kadam@raweng.com>
2018-10-26 21:01:45 +0530

    react app created

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+        <div class="container">
+            <p>Built.io joins Software AG! Read all about the news.</p>
+            <a class="cta" href="#" id="ribbon-info">Link</a>
+            <i class="fa fa-close icon-cancel" id="close"></i>
+        </div>,
+  document.getElementById('ribbon-div')
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <div class="col">
+     <p>Communication section</p>
+  </div>,
+document.getElementById('communication-div')
+);
+
+
